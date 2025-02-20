363b4d32ec70c336be5e212b005509077f9c1b71
Malachy Crossan <ma378458@ucf.edu>
2025-02-20 10:54:26 -0500

    Add RCA

---
+++ project/project.srcs/sources_1/new/Part2.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/20/2025 10:47:59 AM
+// Design Name: 
+// Module Name: Part2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Part2(
+    input Cin,
+    input A0,
+    input B0,
+    input A1,
+    input B1,
+    input A2,
+    input B2,
+    input A3,
+    input B3,
+    input A4,
+    input B4,
+    input A5,
+    input B5,
+    input A6,
+    input B6,
+    input A7,
+    input B7,
+    output S0,
+    output S1,
+    output S2,
+    output S3,
+    output S4,
+    output S5,
+    output S6,
+    output S7,
+    output Cout
+    );
+endmodule
