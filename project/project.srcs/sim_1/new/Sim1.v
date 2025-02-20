9b862c2f703bcd68341ef176d89221e97a84a177
Malachy Crossan <ma378458@ucf.edu>
2025-02-20 10:01:02 -0500

    New clean proj

---
+++ project/project.srcs/sim_1/new/Sim1.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/20/2025 10:00:03 AM
+// Design Name: 
+// Module Name: Sim1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Sim1();
+    parameter NUM_INP = 5;
+    reg A, B, C, D, E;
+    reg[NUM_INP-1:0] cnt; 
+    wire F;
+    integer i;
+    Part_1 UUT(.A(A), .B(B), .C(C), .D(D), .E(E), .F(F));
+    initial begin
+        cnt = 0;
+        for(i = 0; i < 2**NUM_INP; i = i + 1) begin
+            A = cnt[4];
+            B = cnt[3];
+            C = cnt[2];
+            D = cnt[1];
+            E = cnt[0];
+            cnt = cnt + 1;
+            #10;
+        end
+    end
+endmodule
---
+++ project/project.srcs/sources_1/new/Part1.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/20/2025 09:59:39 AM
+// Design Name: 
+// Module Name: Part1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Part1(
+    input A,
+    input B,
+    input C,
+    input D,
+    input E,
+    input F,
+    output X,
+    output Y,
+    output Z
+    );
+endmodule
